6b70a1c6f578b861bfffa8730521a182bd98a6a4
Minh311 <86767723+Minh311@users.noreply.github.com>
2021-07-01 16:56:18 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    introTitle: 'Ê Hà ơi',
+    introDesc: `Ấn vào t bảo`,
+    btnIntro: 'OK',
+    title: 'Mai đạp xe vs lên HN nhé',
+    desc: ' Chỉ hơn 50km thôi :vvv !',
+    btnYes: 'Ok luôn k nói nhiều',
+    btnNo: 'Không, mỏi chân lắm',
+    question:'Ok, vậy 6h sáng t qua đón m nha',
+    btnReply: 'Send',
+    reply: 'Đương nhiên rồi, đón nhanh t còn chờ',
+}
